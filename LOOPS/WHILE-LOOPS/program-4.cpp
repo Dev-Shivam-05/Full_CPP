2cb6792f5ef49e44eca3c958a6cb66bf8326fb98
Dev-Shivam-05 <shivambhadoriya1605@gmail.com>
2025-04-09 11:52:19 +0530

    Fifth commit

--- LOOPS/WHILE-LOOPS/program-4.cpp
+++ LOOPS/WHILE-LOOPS/program-4.cpp
@@ -5,12 +5,19 @@ using namespace std;
 
 int main() {
 
-    int num_1;
+    int num_1,num,sum = 0;
 
     cout << "Enter A Number : ";
     cin >> num_1;
 
-    
+    num = num_1;
+
+    while(num_1 != 0)
+    {
+        sum = sum + num_1 % 10;
+        num_1 = num_1 / 10;
+    }
+    cout << "The Sum Of All Digit Of " << num << " Is " << sum << endl;
 
     return 0;
 }
