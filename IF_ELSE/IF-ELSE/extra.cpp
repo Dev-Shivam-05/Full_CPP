9ab7718b0e39982d7885e0ef111c300f7cd4e11a
Dev-Shivam-05 <shivambhadoriya1605@gmail.com>
2025-04-27 12:00:00 +0530

    vote checker

---
+++ IF_ELSE/IF-ELSE/extra.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout << "Enter Your Age :- ";
+    cin >> age;
+
+    if()
+    {
+cout << "Your Age Is " << age << " Above 18 And You're eligible for Voting";
+    }
+    else
+    {
+        cout << "Your Age Is " << age << " You Are Not Eligible For Voting";
+    }
+    return 0;
+}
